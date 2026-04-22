4a9158ce45c4e2af5eceb1365a3a8e4ae22f2842
tanvimori1811 <tanvimori70454091@gmail.com>
2026-04-22 11:23:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import re
 
 app = Flask(__name__)
@@ -12,9 +12,13 @@ def answer():
 
     if len(nums) == 2:
         result = nums[0] + nums[1]
-        return f"The sum is {result}.".strip()
+        return jsonify({
+            "output": f"The sum is {result}."
+        })
 
-    return "I don't know"
+    return jsonify({
+        "output": "I don't know"
+    })
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
